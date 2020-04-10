15a325c4ff20874c171d69a7119fb5718aa78a16
Jared Engler <jared.lite@gmail.com>
2020-04-09 21:42:51 -0500

    dos2unix and format MyTest.cpp

--- MyTest.cpp
+++ MyTest.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
     BigInt a, b, x, y;
 
     // accepts zero case w/o seg fault
@@ -39,4 +40,5 @@ int main(int argc, char *argv[]) {
     cout << x.toString() << endl;
 
     return 0;
+
 }
